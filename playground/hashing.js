dc30e1ef8771bb5ea3db85d10406ee144ca57071
santhosh2514 <46202799+santhosh2514@users.noreply.github.com>
2019-01-31 00:41:14 +0530

    Hash user passwords before saving

--- playground/hashing.js
+++ playground/hashing.js
@@ -1,14 +1,30 @@
 const {SHA256} = require('crypto-js');
 const jwt= require('jsonwebtoken');
-var data = {
-id:10
-}
-var token = jwt.sign(data,'123abc')
-console.log(token);
-var decoded = jwt.verify(token,'123abcc')
-console.log('decoded: ',decoded);;
+const bcrypt = require('bcryptjs')
 
-var message ="I am number 3";
-var hash=SHA256(message).toString();
-console.log(`message: ${message}`);
-console.log(`hash: ${hash}`);
+var password = '123abc'
+
+bcrypt.genSalt(10,(err,salt)=>{
+  bcrypt.hash(password,salt,(err, hash)=>{
+    // console.log(hash);
+  })
+})
+
+var hashedPassword = '$2a$10$TMQDbUfzrMLnshGKkykMcu6f4mReB1CjY11BQojzj24JFtphPjLUG'
+
+bcrypt.compare('123jhgf',hashedPassword,(err,res)=>{
+  console.log(res);
+});
+ //10  is the number of rounds to make a salt.
+// var data = {
+// id:10
+// }
+// var token = jwt.sign(data,'123abc')
+// console.log(token);
+// var decoded = jwt.verify(token,'123abcc')
+// console.log('decoded: ',decoded);;
+//
+// var message ="I am number 3";
+// var hash=SHA256(message).toString();
+// console.log(`message: ${message}`);
+// console.log(`hash: ${hash}`);
